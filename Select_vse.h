8994ffd71aa27c76484fc3afd58c8b412a0c444a
Yasya11 <71263577+Yasya11@users.noreply.github.com>
2020-09-14 15:17:34 +0300

    Create Select_vse.h

---
+++ Select_vse.h
@@ -0,0 +1,17 @@
+#include <cstdio>
+#include <cstdlib>
+#include <string>
+#include <fstream>
+#include <cmath>
+#include <vector>
+#include <ctime>
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "SelectSotr.h"
+#include "SelectSp.h"
+
+using namespace std;
+
+
